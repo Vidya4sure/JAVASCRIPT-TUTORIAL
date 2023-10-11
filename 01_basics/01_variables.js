36cdd4e07df871960f54447792eed52ec052d7e4
vidya <hansdavsh@gmail.com>
2023-10-11 20:23:28 +0530

    datatype and variable

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,7 +1,19 @@
-const accountId = 144553  // cannot change with this keyboard 
+// Basic of javascript
+
+
+/*
+## [Variables] : named container for storing data (Values).
+ 
+*/
+
+let temp  // assigned (undefined)
+const accountId = 144553  // cannot change with this keyboard because [const]
 let accountEmail = "hansdavsh@gmail.com"  
-var accountPassword = "MAWAYSHIguywala@03"
+var accountPassword = "MAWAYSHIdji@03" // avoid using [var]
 let accountState 
+
+
+
 
 accountCity = "Ranchi" // Prefer not to declare by this type 
 
@@ -14,9 +26,10 @@ accountCity = "Bokaro"
 console.log(accountId);
 
 /*
-Prefer not to use var 
+Prefer not to use [var] 
 because of issue in block scope and functional scope 
 */
 
 
 console.table([accountId , accountEmail , accountPassword , accountCity , accountState ])
+// By using above console.table the output arranged in table order.
---
+++ 01_basics/02_dataypes.js
@@ -0,0 +1,61 @@
+ // --[DATA TYPES]--
+
+"use strict" ; // treat all JS Code as newer version 
+
+// alert (3 + 3) // we are using nodejs , not browser
+
+
+// Always write readable code.
+console.log(3+3) // Output = 6
+console.log("Vidya Sagar Hansda") // Output my name
+
+let name = "vidya"  // string data type
+let age = 22        // integer data type 
+let isLoggedIn = false  // boolean data type 
+let state; // this is undefined the value is not assigned
+
+// vlaue output [number]
+console.log(typeof age)
+
+// type of null is an object 
+console.log(typeof null);
+
+// type of undefined is => undefined
+console.log(typeof undefined);
+
+console.log(state) 
+ 
+// These are primitive data type 
+//  - number range 2 to power 53
+//  - bigInt 
+//  - string => ""
+//  - boolean => true/false 
+//  - null => stand alone value 
+//  - undefined 
+//  - symbol => unique
+//  - object 
+
+
+
+/*
+## According to ECMAScript there are 6 primitive data type and 1 non primitive object.
+ -- [Primitive]
+ - Number 
+ - String 
+ - Boolean 
+ - Undefined : represent a variable whose value is not assigned 
+ - Null : represent the intentional absence of value.
+ - Symbol : represents a unique value.
+ 
+ -- [non-primitive]
+ - Object : represent key-value pair
+
+ ## All of them except Object have immutable values, i.e values which cannot 
+    be changed.
+*/
+
+
+
+
+//assignment to do 
+// go to ecma scritpt and read overview about above topics 
