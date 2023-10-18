66c5930bec5639b2b8b07d1bed0e4af3933ff5dc
vidya <hansdavsh@gmail.com>
2023-10-18 22:34:19 +0530

    Add Stack and Heap memory

---
+++ 01_basics/05_memory.js
@@ -0,0 +1,32 @@
+// [MEMORY MANGEMENT IN JAVASCRIPT]
+
+/*
+ # There are two types of memories 
+ - Stack memory  (used by Primitive data type) [You got a 'Copy']
+ - Heap memory   (used by Non-Primitive data type) [you got a 'Reference']
+*/
+
+let myYoutubeName = "Vidya Sagar Hansda"
+
+
+// Here this is [Primitive]
+
+let anotherName = myYoutubeName
+anotherName = "MawayshiGuywala"
+
+console.log(anotherName);
+console.log(myYoutubeName);
+
+// Here this is [Non-Primitive]
+// object example
+let userOne = {  
+    email: "hansdavsh@google.com" ,
+    upi: "user@ybl"
+}
+
+let userTwo = userOne 
+
+userTwo.email = "mawayshi@google.com" 
+
+console.log(userOne);
+console.log(userTwo);
