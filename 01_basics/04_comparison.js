54d50f51e2d89f4b5aa9758f4ac423667a2ffb0f
vidya <hansdavsh@gmail.com>
2023-10-20 20:12:39 +0530

    Add more comparison points

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -1,9 +1,11 @@
 /*
- [COMPARISON]
+ [COMPARISON] OPERATIONS 
 */
 
 // The value comes in boolean and these comparison are easy to understand
 
+
+// Here basic stuff this value is greater / equal / smaller / greater equal than this value
 console.log(2 > 1);
 console.log(2 >= 1);
 console.log(2 <= 1);
@@ -13,14 +15,16 @@ console.log(2 != 1);
 
 // Problem comes where we compare two different datatypes 
 console.log("Problems come from here");
+
+// [CANNOT COMPARE BETWEEN TWO DIFFERENT DATA TYPE]
 console.log("2" > 1);
 console.log("02" > 1);
 // console.log(1 = "1");
 
 
 /*
-    The reason is that an equality check == and comparison >< >= <= 
-    work differently.
+    The reason is that an equality check == and comparison >< >= <= [For reason read ECMAscript]
+    work differently. 
 
     Comparisons convert null to a number,treating it as 0.
     That's why (3) null >= 0 is true and (1) null > 0 is false
