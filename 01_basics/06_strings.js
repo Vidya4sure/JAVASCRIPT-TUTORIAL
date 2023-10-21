1f7e45bfb37c301f7790c2b6c91f3a698fa1df32
vidya <hansdavsh@gmail.com>
2023-10-21 23:24:49 +0530

    initial String notes

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,15 @@
+// [STRINGS]
+
+
+// We can declare String in two ways :
+// #1
+const name = "vidya"
+const repoCout = 50
+
+//#2
+const gameName = new String ('vidya4sure')
+
+console.log(name + repoCout + " Value"); // Not used in industries // Outdated syntax
+
+// Modern syntax // Backticks // String interpulation 
+console.log(`Hello my name is ${name} and my repo cout is ${repoCout}`);
